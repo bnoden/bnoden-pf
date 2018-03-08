220e4901ee3b7f250114f796f726536981c0462a
bnoden <brandroid369@gmail.com>
2018-03-07 23:39:34 -0600

    add express dev server

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,6 +11,25 @@ import { Current, Old, Fun, Concept, Misc } from './ProjectIndex';
 import './style/App.css';
 
 class App extends Component {
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
   render() {
     return (
       <div style={appStyle}>
@@ -37,6 +56,9 @@ class App extends Component {
             </strong>
           </p>
         </div>
+        <p className="App-intro">
+          {console.log(this.state.response)}
+        </p>
       </div>
     );
   }
