7db1a465a5da0bc85be78f3c3223be9456ce3a6e
bnoden <brandroid369@gmail.com>
2018-03-09 13:45:57 -0600

    .

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () =>
   <div className="home">
-    <h1>Welcome to the Development Archives of Brandon Oden</h1>
+    <h1>The Development Archives of Brandon Oden</h1>
   </div>;
 
 export default Home;
