b59c217766a1b6e0f62408ff0177659242350cf0
bnoden <brandroid369@gmail.com>
2018-04-02 22:44:05 -0500

    func

--- client/src/components/access.js
+++ client/src/components/access.js
@@ -1,25 +1,16 @@
-const isMS =
+const isMS = () =>
   /MSIE 10/i.test(navigator.userAgent) ||
   /MSIE 9/i.test(navigator.userAgent) ||
   /rv:11.0/i.test(navigator.userAgent) ||
   /Edge\/\d./i.test(navigator.userAgent)
-    ? true
-    : false;
+    ? 1
+    : 0;
 
 export const browserIs = {
-  Chrome: () => {
-    return !isMS && !!window.chrome ? true : false;
-  },
-  Firefox: () => {
-    return navigator.userAgent.toLowerCase().indexOf('firefox') > -1
-      ? true
-      : false;
-  },
-  MS: () => {
-    return isMS ? true : false;
-  }
+  Chrome: () => (!isMS() && !!window.chrome ? 1 : 0),
+  Firefox: () =>
+    navigator.userAgent.toLowerCase().indexOf('firefox') > -1 ? 1 : 0,
+  MS: () => (isMS() ? 1 : 0)
 };
 
-export const crYear = (dt = new Date()) => {
-  return dt.getFullYear();
-};
+export const crYear = (dt = new Date()) => dt.getFullYear();
