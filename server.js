ddf1155c3e8076efc8a03c3d15780f3f0e43f2df
Brandon Oden <brandon@bnoden.com>
2018-11-16 03:16:26 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const routes = require('./src/routes');
-const exphbs = require('express-handlebars');
-const nodemailer = require('nodemailer');
+
+const routes = require('./routes');
 
 const app = express();
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 5000;
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/pftest_test');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
-app.post('/api/form', (req, res) => {
-  nodemailer.createTest;
+app.get('/api', (req, res) => {
+  res.send({ express: 'App is running from server.js' });
 });
-
-app,
-  get('/', (req, res) => {
-    res.send({ express: 'App is running from server.js' });
-  });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
